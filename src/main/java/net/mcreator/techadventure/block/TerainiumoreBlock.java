781f48e10572164b2d478df0bbf7db1669e41840
Anton N. Kuhay <mamin.snook@gmail.com>
2021-07-15 16:01:46 -0400

    Pushing initial state of the mod. Will keep pushing inline until I get collaborators.
    I made minor changes in the MC.

--- src/main/java/net/mcreator/techadventure/block/TerainiumoreBlock.java
+++ src/main/java/net/mcreator/techadventure/block/TerainiumoreBlock.java
@@ -106,8 +106,8 @@ public class TerainiumoreBlock extends TechAdventureModElements.ModElement {
 					return super.generate(world, generator, rand, pos, config);
 				}
 			};
-			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 5)).range(30)
-					.square().func_242731_b(7);
+			configuredFeature = feature.withConfiguration(new OreFeatureConfig(CustomRuleTest.INSTANCE, block.getDefaultState(), 4)).range(30)
+					.square().func_242731_b(4);
 			event.getRegistry().register(feature.setRegistryName("terainiumore"));
 			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("tech_adventure:terainiumore"), configuredFeature);
 		}
