4e137e2b31689ac79a375a7842de76f1a2d13e99
Anton N. Kuhay <mamin.snook@gmail.com>
2021-09-02 02:26:37 -0400

    tech_adventure-4 Produce the next batch of experimental features.
    Added last generated wrapper

--- strip.sh
+++ strip.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-rm -rf .DS_Store .classpath .gradle .idea .mcreator .project .settings build gradle gradlew gradlew.bat run
+rm -rf .DS_Store .classpath .gradle .idea .mcreator .project .settings build run
